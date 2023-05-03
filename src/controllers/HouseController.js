6e5fa63b2f3089bda67a887e0c61673297a662b5
Luiz Silva <lfdjs0000@gmail.com>
2023-05-03 17:57:04 -0300

    Atualização

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,6 @@
 import House from "../models/House";
 import User from "../models/User";
-
+import * as Yup from 'yup';
 class HouseController {
 
     // Listar Casas
@@ -15,9 +15,21 @@ class HouseController {
     // Cadastrar casas
     async store(req, res) {
 
+        const schema = Yup.object().shape({
+            description: Yup.string().required(),
+            price: Yup.number().required(),
+            location: Yup.string().required(),
+            status: Yup.boolean().required(),
+        });
+
+
         const { filename } = req.file;
         const { description, price, location, status } = req.body;
         const { user_id } = req.headers;
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: "Falha na validação" })
+        }
 
         const house = await House.create({
             user: user_id,
@@ -33,11 +45,21 @@ class HouseController {
 
     // Alterar casas
     async update(req, res) {
+        const schema = Yup.object().shape({
+            description: Yup.string().required(),
+            price: Yup.number().required(),
+            location: Yup.string().required(),
+            status: Yup.boolean().required(),
+        });
 
         const { filename } = req.file;
         const { description, price, location, status } = req.body;
         const { user_id } = req.headers;
         const { house_id } = req.params;
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: "Falha na validação" })
+        }
 
         const user = await User.findById(user_id);
         const houses = await House.findById(house_id);
@@ -69,7 +91,7 @@ class HouseController {
             return res.status(401).json({ error: "Não autorizado" });
         }
 
-        await House.findByIdAndDelete({_id: house_id})
+        await House.findByIdAndDelete({ _id: house_id })
 
         return res.json({ message: "Excluida com sucesso" });
     }
--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import * as Yup from 'yup';
 
 // Métodos: index, show, update, store, destroy
 
@@ -13,7 +14,15 @@ destroy: quando queremos deletar uma sessão
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        });
+
         const { email } = req.body;
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: "Falha na validação" })
+        }
 
         //Verificando se este usuário ja existe
         let user = await User.findOne({ email });
